ae8706d6d4a8313995ba37ca4179d8fde4c477b0
Jean Yves Hector <jeanyveshector@gmail.com>
2025-03-16 16:15:11 -0500

    Initial commit

--- server.py
+++ server.py
@@ -77,7 +77,7 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [settings.client_url, "http://localhost:3000"] if settings.client_url else ["http://localhost:3000"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
