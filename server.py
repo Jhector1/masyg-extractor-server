2841375e097fe74209b90eb90e237c85afdbf8ce
Jean Yves Hector <jeanyveshector@gmail.com>
2025-04-16 11:58:27 -0500

    Major change from session to jwt

--- server.py
+++ server.py
@@ -125,7 +125,7 @@ if ENV == "production":
     app.add_middleware(
         DefaultCookieMiddleware,
         default_domain=".masyglink.com",
-        default_samesite="strict",
+        default_samesite="none",
         default_secure=True,
         default_httponly=True,
         default_max_age=1800  # e.g., 30 minutes
